c915fa3dcc2c083516b0fab82e4ea6bb9325221f
Caleb Oliphant <caleb.oliphant@gmail.com>
2023-12-21 14:51:27 -0600

    Getting hot reloading to work i think...?

--- sigmashortcuts/src/App.js
+++ sigmashortcuts/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import MathJax from 'react-mathjax';
 
 function App() {
+  const inlineFormula = `k_{n+1} = n^2 + k_n^2 - k_{n-1}`;
+  const blockFormula = `\\int_0^\\infty x^3 dx`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+    <MathJax.Provider>
+      <div>
+        <p>Inline formula: <MathJax.Node inline formula={inlineFormula} /></p>
+        <hr></hr>
+        <p>Block formula:</p>
+        <MathJax.Node formula={blockFormula} />
+      </div>
+    </MathJax.Provider>
+  </div>
   );
 }
 
